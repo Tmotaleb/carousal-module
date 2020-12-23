1919ba847be681f6b8843787cd4c617e6a994bae
Tamanna Motaleb <Tamannamotaleb@gmail.com>
2020-12-22 20:33:40 -0500

    completed footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,7 +7,26 @@ import regeneratorRuntime from 'regenerator-runtime';
 var Footer = () => {
 
   return (
-    <style.footer></style.footer>
+    <style.footer>
+    <ol>
+      <ul className='footer-ul'>
+        <li><a href='#' className='fa fa-facebook'></a></li>
+        <li><a href='#' className='fa fa-twitter'></a></li>
+        <li><a href='#' className='fa fa-linkedin'></a></li>
+        <li><a href='#' className='fa fa-instagram'></a></li>
+        <li><a href='#' className='fa fa-youtube'></a></li>
+        <li><a href='#' className='fa fa-pinterest'></a></li>
+      </ul>
+
+      <li className='list2'>
+        <p className='footer-brand'>
+          <a href='#'>Terms of Use</a> | <a href='#'>Privacy Policy</a>
+          <br/>
+          Work'n Treehouse, Inc. 2020
+        </p>
+        </li>
+    </ol>
+    </style.footer>
   )
 }
 
