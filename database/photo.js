a0fd07ddd3ebb9696ed3cbb83da8120488d0af37
Tamanna Motaleb <Tamannamotaleb@gmail.com>
2021-01-13 14:43:18 -0500

    fixed error in seeding data

--- database/photo.js
+++ database/photo.js
@@ -2,9 +2,11 @@ var coffeeTable = ['https://my-carousal-bucket.s3-us-west-1.amazonaws.com/carous
 
 var studySpace = ['https://my-carousal-bucket.s3-us-west-1.amazonaws.com/more-info-img/morespacepic/image10.jpg','https://my-carousal-bucket.s3-us-west-1.amazonaws.com/more-info-img/morespacepic/image4.jpg','https://my-carousal-bucket.s3-us-west-1.amazonaws.com/more-info-img/morespacepic/image6.jpeg','https://my-carousal-bucket.s3-us-west-1.amazonaws.com/more-info-img/morespacepic/image9.jpg','https://my-carousal-bucket.s3-us-west-1.amazonaws.com/carousal-image+folder/carousal-image+folder/studyspace3.jpg','https://my-carousal-bucket.s3-us-west-1.amazonaws.com/carousal-image+folder/carousal-image+folder/studyspace7.jpg','https://my-carousal-bucket.s3-us-west-1.amazonaws.com/carousal-image+folder/carousal-image+folder/studyspace8.jpg'];
 
+var moreSpaces = ['https://my-carousal-bucket.s3-us-west-1.amazonaws.com/more-info-img/morespacepic/194588533-KL-1024x680-1024x680.jpg', 'https://my-carousal-bucket.s3-us-west-1.amazonaws.com/more-info-img/morespacepic/image2.jpg', 'https://my-carousal-bucket.s3-us-west-1.amazonaws.com/more-info-img/morespacepic/image7.jpg', 'https://my-carousal-bucket.s3-us-west-1.amazonaws.com/more-info-img/morespacepic/image5.jpg', 'https://my-carousal-bucket.s3-us-west-1.amazonaws.com/more-info-img/morespacepic/image8.jpeg']
 module.exports = {
   coffeeTable,
-  studySpace
+  studySpace,
+  moreSpaces
 }
 
 
