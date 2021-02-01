f8c1733c659d331691765f74bf9a1a67460ac5de
Tamanna Motaleb <Tamannamotaleb@gmail.com>
2021-02-01 17:20:03 -0500

    fixed saveShare button issue

--- client/src/components/SaveShare.jsx
+++ client/src/components/SaveShare.jsx
@@ -5,14 +5,19 @@ import {HiShare} from 'react-icons/Hi';
 
 var SaveShare = () => {
 
-  const [active, setActive] = useState(false)
-  const toggle = () => setActive(!active);
+  const [activeSave, setActiveSave] = useState(false);
+
+  const [activeShare, setActiveShare] = useState(false);
+
+  const toggle1 = () => setActiveSave(!activeSave);
+  const toggle2 = () => setActiveShare(!activeShare)
 
   return (
   <div className='buttonBar'>
-     <div className='saveButton'><ImHeart onClick={toggle} className={active ? 'heart-icon is-clicked .bouncy' : 'heart-icon not-clicked'}/></div>
 
-     <div className='shareButton'><HiShare onClick={toggle} className={active ? 'share-icon did-click .bounce' : 'share-icon didnt-click'}/></div>
+     <div className='saveButton'><ImHeart onClick={toggle1} className={activeSave ? 'heart-icon is-clicked .bouncy' : 'heart-icon not-clicked'}/></div>
+
+     <div className='shareButton'><HiShare onClick={toggle2} className={activeShare ? 'share-icon did-click .bounce' : 'share-icon didnt-click'}/></div>
   </div>
 
   )
