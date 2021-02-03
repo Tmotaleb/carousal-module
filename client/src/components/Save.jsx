e5fbefc2018fe371c13a8100da1851ea95f63def
Tamanna Motaleb <Tamannamotaleb@gmail.com>
2021-02-03 18:46:11 -0500

    added content to popup modal for save button

--- client/src/components/Save.jsx
+++ client/src/components/Save.jsx
@@ -1,14 +1,79 @@
 import React, {useState, useEffect} from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import {ImHeart} from 'react-icons/Im';
+import {FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fabFacebook } from "@fortawesome/free-solid-svg-icons";
+import {SiFacebook} from 'react-icons/Si';
+import {AiFillTwitterCircle} from 'react-icons/Ai';
+import {FcGoogle} from 'react-icons/Fc';
 
 var Save = () => {
   const [activeSave, setActiveSave] = useState(false);
   const toggle1 = () => setActiveSave(!activeSave);
 
   return (
-     <div className='saveButton'>
-      <ImHeart onClick={toggle1, handleShow} className={activeSave ? 'heart-icon is-clicked .bouncy' : 'heart-icon not-clicked'}/>
-    </div>
+    <Popup trigger={
+      <div className='saveButton'>
+        <ImHeart onClick={toggle1} className={activeSave ? 'heart-icon is-clicked .bouncy' : 'heart-icon not-clicked'}/>
+      </div>
+      }
+      modal
+      nested
+      >
+      {close => (
+        <div className='popup-modal'>
+          <button className='close' onClick={close}>&times;</button>
+
+          <div className='header'>
+            <h1>Work'n Treehouse</h1>
+            <h3>Sign in or register to save your favorite workspaces</h3>
+          </div>
+
+          <div className='content'>
+            <form>
+              <div className='contentainer'>
+                <label for='email'><b>Email</b></label>
+                <input type='text' placeholder='Enter email address' name='email' id='email' required/>
+
+                <label for='psw'><b>Password</b></label>
+                <input type='password' placeholder='Enter password, 8-15 characters' name='psw' id='psw' required/>
+
+                <button type='submit' class='registerbtn'>Register</button>
+
+                <p>I accept Work'n Treehouse's <a href='#'>Terms of Use</a> and <a href='#'>Privacry Policy.</a></p>
+              </div>
+
+                <div className='social-col'>
+                  <a href='#' className='fb btn'>
+                    <SiFacebook className='facebook'/>Continue with Facebook
+                  </a>
+
+                  <a href='#' className='twt btn'>
+                    <AiFillTwitterCircle className='twitter'/> Continue with Twitter
+                  </a>
+
+                  <a href='#' className='gle btn'>
+                    <FcGoogle className='google'/> Continue with Google
+                  </a>
+
+
+                </div>
+
+              <div className='contentainer signin'>
+                <p>Already have an account? <a href='#'>Sign in</a>.</p>
+              </div>
+
+            </form>
+          </div>
+{/*
+          <button className='button' onClick={() => {close()}}>Close</button> */}
+        </div>
+      )
+
+      }
+
+   </Popup>
   )
 }
 
