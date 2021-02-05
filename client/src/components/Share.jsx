3869c41dcf222da0c2f12a22446c9534d883cfd6
Tamanna Motaleb <Tamannamotaleb@gmail.com>
2021-02-04 19:50:30 -0500

    working on share modal

--- client/src/components/Share.jsx
+++ client/src/components/Share.jsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {HiShare} from 'react-icons/Hi';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 var Share = () => {
 
@@ -8,9 +10,43 @@ var Share = () => {
   const toggle2 = () => setActiveShare(!activeShare)
 
   return (
-    <div className='shareButton'>
-      <HiShare onClick={toggle2} className={activeShare ? 'share-icon did-click .bounce' : 'share-icon didnt-click'}/>
-    </div>
+    <Popup trigger={
+      <div className='shareButton'>
+        <HiShare onClick={toggle2} className={activeShare ? 'share-icon did-click .bounce' : 'share-icon didnt-click'}/>
+      </div>
+      }
+      modal
+      nested
+    >
+    {close => (
+      <div className='share-modal'>
+        <button className='sh-close-btn' onClick={close}>&times;
+        </button>
+
+        <div className='share-header'>
+          Share this place with family and friends
+        </div>
+
+        <div className='share-content'>
+           <ul className='share-ul'>
+            <li><p>Share via: </p></li>
+            <li><a href='#' id='share-icon' className='fa fa-facebook'></a></li>
+
+            <li><a href='#' id='share-icon'  className='fa fa-twitter'></a></li>
+
+            <li><a href='#' id='share-icon'  className='fa fa-linkedin'></a></li>
+
+            <li><a href='#' id='share-icon'  className='fa fa-instagram'></a></li>
+
+            <li><a href='#' id='share-icon'  className='fa fa-youtube'></a></li>
+
+            <li><a href='#' id='share-icon'  className='fa fa-pinterest'></a></li>
+          </ul>
+        </div>
+      </div>
+    )}
+
+    </Popup>
   )
 }
 
